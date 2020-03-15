547062424fc744979fd618a457f6e5d0f72b3abb
thesmartassole <32884516+thesmartassole@users.noreply.github.com>
2020-03-15 15:28:17 -0100

    Update Main.java

---
+++ Main.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class Main
+{
+    public static List<String> argsL = null;
+    public static final String RECURSION_FLAG = "-r";
+
+    public static void main(String[] args) 
+    {
+        argsL = Arrays.asList(args);
+    }   
+
+    public static int drinaProfitR(int[][][][] offers)
+    {
+        
+    }
+
+    public static int drinaProfitDP(int[][][][] offers)
+    {
+        
+    }
+}
