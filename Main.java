deddf02b03ab57da48809ac6fa79ce32730d1642
thesmartassole <32884516+thesmartassole@users.noreply.github.com>
2020-03-17 21:12:27 -0100

    added offer and drina class

--- Main.java
+++ Main.java
@@ -1,52 +1,83 @@
 import java.io.BufferedReader;
+import java.io.FileInputStream;
 import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main
 {
     public static List<String> argsL = null;
-    public static final String RECURSION_FLAG = "-r";
-
-    public static final int STARTING_TIME_INDEX = 0;
-    public static final int DURATION_INDEX = 1;
-    public static final int PRICE_INDEX = 2;
+    public static final String RECURSION_FLAG = "-r", 
+    TEST_INPUT_FLAG = "-t", DURATION_FLAG = "-d", PRINT_MATRIX_FLAG = "-pm";
 
 
-    public static void main(String[] args) 
+    public static void main(String[] args) throws Exception
     {
+        long startTime = System.currentTimeMillis();
         argsL = Arrays.asList(args);
         BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        if (argsL.contains(TEST_INPUT_FLAG))
+        {
+            in = getTestInputReader(in);
+            startTime = System.currentTimeMillis();
+        }
         int test_cases = Integer.parseInt(in.readLine());
         List<Integer> results = new LinkedList<Integer>();
         for(int t = 0; t < test_cases; t++)
         {
             int numberOffers = Integer.parseInt(in.readLine());
-            int[][] offers = new int[numberOffers][3];
-            for(int[] o : offers)
+            List<Offer> offers = new ArrayList<Offer>(numberOffers);
+            for(int i = 0; i < numberOffers; i++)
             {
                 String[] s = in.readLine().split(" ");
-                offers[t][STARTING_TIME_INDEX] = Integer.parseInt(s[STARTING_TIME_INDEX]);
-                offers[t][DURATION_INDEX] = Integer.parseInt(s[DURATION_INDEX]);
-                offers[t][PRICE_INDEX] = Integer.parseInt(s[PRICE_INDEX]);
+                offers.add(new Offer(Integer.parseInt(s[0]),
+                                     Integer.parseInt(s[1]),
+                                     Integer.parseInt(s[2])));
             }
-            // results.add(1);
+            int result = new Drina(offers).solveDP();
+            results.add(result);
         }
+        long endTime = System.currentTimeMillis();
+        if(argsL.contains(DURATION_FLAG))
+            System.out.println("Duration: " + (endTime - startTime) + " ms");
     }   
 
-    public static int drinaProfitR(int[][] offers, int currentOffer)
+    public static class Drina
     {
-        int endingTime = offers[currentOffer][STARTING_TIME_INDEX] + offers[currentOffer][DURATION_INDEX];
-        for(int i = 0; i < offers.length; i++)
+        public final List<Offer> offers;
+        private int[] matrix;
+
+        public Drina(List<Offer> offers)
         {
-            if (i != currentOffer)
-            {
-                
-            }
+            this.offers = offers;
+        }
+
+        public int solveDP()
+        {
+            throw new Error("Not implemented");
+        }
+
+    }
+
+    public static class Offer
+    {
+        public final int duration;
+        public final int startingTime;
+        public final int price;
+
+        public Offer(int startingTime, int duration, int price)
+        {
+            this.startingTime = startingTime;
+            this.duration = duration;
+            this.price = price;
         }
     }
 
-    public static int drinaProfitDP(int[][][][] offers)
+
+    private static BufferedReader getTestInputReader(BufferedReader in) throws Exception
     {
-        
+        System.out.print("Insert test filename: ");
+        String file = in.readLine();
+        in.close();
+        return new BufferedReader(new InputStreamReader(new FileInputStream(file)));
     }
 }
